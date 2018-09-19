f145e84e30d488ff802f3e0f778f6eeaa2116e88
John Sime <johnc.sime@gmail.com>
2018-09-19 04:43:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,29 +1,32 @@
-// add solution here
 function theBeatlesPlay(musicians, instruments) {
-  var array = [];
-  for (var i = 0; i < musicians.length; i++) {
-    array.push(musicians[i] + " plays " + instruments[i]);
+  const arr = []
+
+  for (let i = 0, l = musicians.length; i < l; i++) {
+    arr.push(`${musicians[i]} plays ${instruments[i]}`)
   }
-  return array;
+
+  return arr
 }
 
-function johnLennonFacts() {
-  var facts = [ "He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice" ];
-  var array = [];
-  var i = 0;
-  while (array.length < facts.length) {
-    array.push(facts[i] + "!!!");
-    i++;
+function johnLennonFacts(facts) {
+  const shoutedFacts = []
+
+  let i = 0
+  while (i < facts.length) {
+    shoutedFacts.push(`${facts[i]}!!!`)
+    i++
   }
-  return array;
+
+  return shoutedFacts
 }
 
+function iLoveTheBeatles(n) {
+  const arr = []
 
-function iLoveTheBeatles(number) {
-  var array = [];
   do {
-    array.push("I love the Beatles!");
-    number += 1
-  } while (number < 15);
-  return array;
+    arr.push('I love the Beatles!')
+    n++
+  } while (n < 15)
+
+  return arr
 }
