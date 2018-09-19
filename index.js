db552354d8a338abd56a653c6429ded51118f5c9
John Sime <johnc.sime@gmail.com>
2018-09-19 04:21:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function johnLennonFacts() {
 }
 
 function johnLennonFacts() {
-  var facts = ['He was the last Beatle to learn to drive', 'He was never a vegetarian', 'He was a choir boy and boy scout', 'He hated the sound of his own voice', 'foo', 'bar'];
+  var facts = [ 'He was the last Beatle to learn to drive', 'He was never a vegetarian', 'He was a choir boy and boy scout', 'He hated the sound of his own voice', 'foo', 'bar'];
   var array = [];
   var i = 0;
   while (array.length < facts.length) {
